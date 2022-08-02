ab426e440e01ed5d3c90e70d2a8c4aac372a46e9
Amit2026 <Amit.yadav117019@gmail.com>
2022-08-02 11:43:04 +0530

    Formatted

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -30,7 +30,6 @@ class Item extends React.Component {
     );
   }
 
-
   componentWillUnmount() {
     this.props.onUnload();
   }
@@ -39,7 +38,7 @@ class Item extends React.Component {
     if (!this.props.item) {
       return null;
     }
-    
+
     const markup = {
       __html: marked(this.props.item.description, { sanitize: true }),
     };
@@ -52,7 +51,9 @@ class Item extends React.Component {
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image ? this.props.item.image : placeholder}
+                src={
+                  this.props.item.image ? this.props.item.image : placeholder
+                }
                 alt={this.props.item.title}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
